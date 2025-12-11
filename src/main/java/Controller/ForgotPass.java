e3df9f9d939a7cc90c5204079b7b2e61dcecee55
BaoTam368 <23130286@st.hcmuaf.edu.vn>
2025-12-11 21:00:14 +0700

    tái cấu trúc toàn bộ code

---
+++ src/main/java/Controller/ForgotPass.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ForgotPass", value = "/ForgotPass")
+public class ForgotPass extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/Views/ForgotPass.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
