cbe3bb07824bab9a50968cc746878b4cecd1fe46
BaoTam368 <23130286@st.hcmuaf.edu.vn>
2026-01-28 10:01:55 +0700

    Cập nhật css cho event và tìm kiếm

--- src/main/java/Controller/AdminProduct.java
+++ src/main/java/Controller/AdminProduct.java
@@ -1,45 +1,20 @@
 package Controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
-import Model.DAO.ProductDAO;
-import Model.Object.Product;
-
-
-@WebServlet("/AdminProduct")
+@WebServlet(name = "AdminProduct", value = "/AdminProduct")
 public class AdminProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/Views/Admin/AdminProduct.jsp").forward(request, response);
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        ProductDAO dao = new ProductDAO();
-        String action = req.getParameter("action");
-
-        if ("delete".equals(action)) {
-            int id = Integer.parseInt(req.getParameter("id"));
-            dao.delete(id);
-            resp.sendRedirect(req.getContextPath() + "/admin/product");
-            return;
-        }
-
-        if ("edit".equals(action)) {
-            int id = Integer.parseInt(req.getParameter("id"));
-            Product p = dao.getProductById(id);
-            req.setAttribute("product", p);
-            req.getRequestDispatcher("/AdminEditProduct.jsp").forward(req, resp);
-            return;
-        }
-
-        List<Product> list = dao.getAllProductsAdmin();
-        req.setAttribute("products", list);
-        req.getRequestDispatcher("/WEB-INF/Views/Admin/AdminProduct.jsp").forward(req, resp);
     }
 }
